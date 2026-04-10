9fde5d2a1eec103b93e3a87245c42c9f2688098c
Soren Rasmussen <s.c.rasmussen@gmail.com>
2026-04-10 16:03:28 -0600

    Adding MPAS init, run, finalize to NUOPC cap
    Co-authored-by: Daniel Rosen <drosen@ucar.edu>

--- src/core_atmosphere/nuopc/mpas_nuopc_atm.F90
+++ src/core_atmosphere/nuopc/mpas_nuopc_atm.F90
@@ -13,9 +13,34 @@ module mpas_nuopc_atm
   use NUOPC_Model, &
     modelSS => SetServices
 
+#ifdef MPAS_USE_MPI_F08
+  use mpi_f08, only : MPI_Comm
+#endif
+
+  ! MPAS modules
+  use mpas_derived_types, only: core_type, domain_type, block_type, &
+    mpas_pool_type, mpas_time_type
+  use mpas_kind_types, only: rkind, r8kind, strkind
+  use mpas_pool_routines, only: mpas_pool_get_config
+  use mpas_subdriver, only: mpas_init, mpas_finalize
+  use atm_core, only: atm_core_run_prepare, atm_core_run_step
+
   implicit none
 
   private
+  ! MPAS state attached to NUOPC model
+  type mpas_nuopc_atm_state
+    type (core_type), pointer :: corelist => null()
+    type (domain_type), pointer :: domain => null()
+    real (kind=rkind), pointer :: dt => null()
+    integer :: itimestep
+    character (len=StrKIND), pointer :: config_restart_timestamp_name => null()
+    logical, pointer :: config_apply_lbcs
+  end type mpas_nuopc_atm_state
+
+  type mpas_nuopc_atm_wrapper
+    type(mpas_nuopc_atm_state), pointer :: mState => null()
+  end type mpas_nuopc_atm_wrapper
 
   public SetVM, SetServices
 
@@ -35,6 +60,9 @@ module mpas_nuopc_atm
     ! arguments
     type(ESMF_GridComp)  :: model
     integer, intent(out) :: rc
+
+    ! local variables
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
 
     rc = ESMF_SUCCESS
 
@@ -62,6 +90,10 @@ module mpas_nuopc_atm
       specRoutine=Finalize, rc=rc)
     ESMF_ERR_RETURN(rc)
 
+    allocate(modelStateWrapper%mState)
+    call ESMF_InternalStateAdd(model, internalState=modelStateWrapper, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
   end subroutine SetServices
 
   !-----------------------------------------------------------------------------
@@ -74,18 +106,42 @@ module mpas_nuopc_atm
     integer, intent(out) :: rc
 
     ! local variables
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
+    type(mpas_nuopc_atm_state), pointer :: mState
+    type(ESMF_VM) :: vm
     type(ESMF_State) :: importState, exportState
+    integer :: int_mpic
+#ifdef MPAS_USE_MPI_F08
+    type(MPI_Comm) :: mpic
+#endif
 
     rc = ESMF_SUCCESS
+
+    call ESMF_InternalStateGet(model, internalState=modelStateWrapper, rc=rc)
+    mState => modelStateWrapper%mState
+    ESMF_ERR_RETURN(rc)
 
     call NUOPC_ModelGet(model, importState=importState, &
       exportState=exportState, rc=rc)
     ESMF_ERR_RETURN(rc)
 
-    call ESMF_LogWrite(logmsgFlag=ESMF_LOGMSG_ERROR, &
-      msg="MPAS NUOPC ATM - Advertise has not been implemented", &
-      line=__LINE__, &
-      file=__FILE__)
+    call ESMF_GridCompGet(model, vm=vm, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+    call ESMF_VMGet(vm, &
+      mpiCommunicator=int_mpic, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+#ifdef MPAS_USE_MPI_F08
+    mpic%mpi_val = int_mpic
+    call mpas_init(corelist=mState%corelist, &
+      domain_ptr=mState%domain, &
+      external_comm=mpic)
+#else
+    call mpas_init(corelist=mState%corelist, &
+      domain_ptr=mState%domain, &
+      external_comm=int_mpic)
+#endif
 
   end subroutine Advertise
 
@@ -99,18 +155,21 @@ module mpas_nuopc_atm
     integer, intent(out) :: rc
 
     ! local variables
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
+    type(mpas_nuopc_atm_state), pointer :: mState
     type(ESMF_State) :: importState, exportState
 
     rc = ESMF_SUCCESS
+
+    call ESMF_InternalStateGet(model, internalState=modelStateWrapper, rc=rc)
+    mState => modelStateWrapper%mState
+    ESMF_ERR_RETURN(rc)
 
     call NUOPC_ModelGet(model, importState=importState, &
       exportState=exportState, rc=rc)
     ESMF_ERR_RETURN(rc)
 
-    call ESMF_LogWrite(logmsgFlag=ESMF_LOGMSG_ERROR, &
-      msg="MPAS NUOPC ATM - Realize has not been implemented", &
-      line=__LINE__, &
-      file=__FILE__)
+    ! (no fields to connect for initial implementation)
 
   end subroutine Realize
 
@@ -124,20 +183,63 @@ module mpas_nuopc_atm
     integer, intent(out) :: rc
 
     ! local variables
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
+    type(mpas_nuopc_atm_state), pointer :: mState
     type(ESMF_Clock) :: clock
+    type(ESMF_TimeInterval) :: timeStep
+    type(ESMF_Time) :: startTime, currentTime, stopTime
     type(ESMF_State) :: importState, exportState
+    integer(ESMF_KIND_I4) :: dt_i
+    character(len=32) :: dateString
 
     rc = ESMF_SUCCESS
+
+    call ESMF_InternalStateGet(model, internalState=modelStateWrapper, rc=rc)
+    mState => modelStateWrapper%mState
+    ESMF_ERR_RETURN(rc)
 
     call NUOPC_ModelGet(model, modelClock=clock, &
       importState=importState, &
       exportState=exportState, rc=rc)
     ESMF_ERR_RETURN(rc)
 
-    call ESMF_LogWrite(logmsgFlag=ESMF_LOGMSG_ERROR, &
-      msg="MPAS NUOPC ATM - SetClock has not been implemented", &
-      line=__LINE__, &
-      file=__FILE__)
+    ! initialize time step (dt)
+    call mpas_pool_get_config(mState%domain%blocklist%configs, &
+      key='config_dt', value=mState%dt)
+    dt_i = int(mState%dt, kind=ESMF_KIND_I4)
+    call ESMF_TimeIntervalSet(timeStep, s=dt_i, rc=rc) ! MPAS dt in seconds
+    ESMF_ERR_RETURN(rc)
+    call NUOPC_CompSetClock(model, clock, timeStep, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+    ! log clock information
+    call ESMF_ClockGet(clock, timeStep=timeStep, currTime=currentTime, &
+      startTime=startTime, stopTime=stopTime, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+    call ESMF_TimeIntervalGet(timeStep, timeStringISOFrac=dateString, rc=rc)
+    ESMF_ERR_RETURN(rc)
+    call ESMF_LogWrite('ESMF timestep: ' // trim(dateString), &
+      ESMF_LOGMSG_INFO, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+    call ESMF_TimeGet(currentTime, timeStringISOFrac=dateString, rc=rc)
+    ESMF_ERR_RETURN(rc)
+    call ESMF_LogWrite("ESMF current time: " // trim(dateString), &
+      ESMF_LOGMSG_INFO, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+    call ESMF_TimeGet(startTime, timeStringISOFrac=dateString, rc=rc)
+    ESMF_ERR_RETURN(rc)
+    call ESMF_LogWrite("ESMF start time: " // trim(dateString), &
+      ESMF_LOGMSG_INFO, rc=rc)
+    ESMF_ERR_RETURN(rc)
+
+    call ESMF_TimeGet(stopTime, timeStringISOFrac=dateString, rc=rc)
+    ESMF_ERR_RETURN(rc)
+    call ESMF_LogWrite("ESMF stop time: " // trim(dateString), &
+      ESMF_LOGMSG_INFO, rc=rc)
+    ESMF_ERR_RETURN(rc)
 
   end subroutine SetClock
 
@@ -151,24 +253,43 @@ module mpas_nuopc_atm
     integer, intent(out) :: rc
 
     ! local variables
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
+    type(mpas_nuopc_atm_state), pointer :: mState
     type(ESMF_Clock) :: clock
     type(ESMF_State) :: importState, exportState
+    integer :: ierr
 
     rc = ESMF_SUCCESS
+
+    call ESMF_InternalStateGet(model, internalState=modelStateWrapper, rc=rc)
+    mState => modelStateWrapper%mState
+    ESMF_ERR_RETURN(rc)
 
     call NUOPC_ModelGet(model, modelClock=clock, &
       importState=importState, &
       exportState=exportState, rc=rc)
     ESMF_ERR_RETURN(rc)
 
+    ierr = atm_core_run_prepare(domain=mState%domain)
+    if (ierr /= 0) then
+      call ESMF_LogSetError(ESMF_FAILURE, &
+        msg="Error in atm_core_run_prepare", &
+        line=__LINE__, &
+        file=__FILE__, &
+        rcToReturn = rc)
+      return
+    end if
+    mState%itimestep = 1
+    call mpas_pool_get_config(mState%domain%blocklist%configs, &
+      key='config_restart_timestamp_name', &
+      value=mState%config_restart_timestamp_name)
+    call mpas_pool_get_config(mState%domain%blocklist%configs, &
+      key='config_apply_lbcs', &
+      value=mState%config_apply_lbcs)
+
     call NUOPC_CompAttributeSet(model, &
       name="InitializeDataComplete", value="true", rc=rc)
     ESMF_ERR_RETURN(rc)
-
-    call ESMF_LogWrite(logmsgFlag=ESMF_LOGMSG_ERROR, &
-      msg="MPAS NUOPC ATM - DataInitialize has not been implemented", &
-      line=__LINE__, &
-      file=__FILE__)
 
   end subroutine DataInitialize
 
@@ -182,20 +303,37 @@ module mpas_nuopc_atm
     integer, intent(out) :: rc
 
     ! local variables
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
+    type(mpas_nuopc_atm_state), pointer :: mState
     type(ESMF_Clock) :: clock
     type(ESMF_State) :: importState, exportState
+    integer :: ierr, stream_dir
+    character(len=strkind) :: input_stream, read_time
+    real (kind=r8kind) :: integ_start_time, integ_stop_time
 
     rc = ESMF_SUCCESS
+
+    call ESMF_InternalStateGet(model, internalState=modelStateWrapper, rc=rc)
+    mState => modelStateWrapper%mState
+    ESMF_ERR_RETURN(rc)
 
     call NUOPC_ModelGet(model, modelClock=clock, &
       importState=importState, &
       exportState=exportState, rc=rc)
     ESMF_ERR_RETURN(rc)
 
-    call ESMF_LogWrite(logmsgFlag=ESMF_LOGMSG_ERROR, &
-      msg="MPAS NUOPC ATM - Advance has not been implemented", &
-      line=__LINE__, &
-      file=__FILE__)
+    ierr = atm_core_run_step(domain=mState%domain, &
+      itimestep=mState%itimestep, dt=mState%dt, &
+      config_restart_timestamp_name=mState%config_restart_timestamp_name, &
+      config_apply_lbcs=mState%config_apply_lbcs)
+    if (ierr /= 0) then
+      call ESMF_LogSetError(ESMF_FAILURE, &
+        msg="Error in atm_core_run_step", &
+        line=__LINE__, &
+        file=__FILE__, &
+        rcToReturn = rc)
+      return
+    end if
 
   end subroutine Advance
 
@@ -209,18 +347,23 @@ module mpas_nuopc_atm
     integer, intent(out) :: rc
 
     ! local variables
-    type(ESMF_State)            :: importState, exportState
+    type(mpas_nuopc_atm_wrapper) :: modelStateWrapper
+    type(mpas_nuopc_atm_state), pointer :: mState
+    type(ESMF_State) :: importState, exportState
 
     rc = ESMF_SUCCESS
+
+    call ESMF_InternalStateGet(model, internalState=modelStateWrapper, rc=rc)
+    mState => modelStateWrapper%mState
+    ESMF_ERR_RETURN(rc)
 
     call NUOPC_ModelGet(model, importState=importState, &
       exportState=exportState, rc=rc)
     ESMF_ERR_RETURN(rc)
 
-    call ESMF_LogWrite(logmsgFlag=ESMF_LOGMSG_ERROR, &
-      msg="MPAS NUOPC ATM - Finalize has not been implemented", &
-      line=__LINE__, &
-      file=__FILE__)
+    call mpas_finalize(corelist=mState%corelist, domain_ptr=mState%domain)
+
+    deallocate(modelStateWrapper%mState)
 
   end subroutine Finalize
 
